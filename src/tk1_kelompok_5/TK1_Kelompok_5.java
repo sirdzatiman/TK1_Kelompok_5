671b99bec4bcb2467bfddef107a6c13bf87b3c8a
sirdzat iman <sirdzatiman80@gmail.com>
2026-09-13 19:38:14 +0700

    Commit Pertama

---
+++ src/tk1_kelompok_5/TK1_Kelompok_5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tk1_kelompok_5;
+
+/**
+ *
+ * @author Sirdzat
+ */
+public class TK1_Kelompok_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
